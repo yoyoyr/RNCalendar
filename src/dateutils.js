dc385428fbc7a7792fbb31d63051390016605373
杨荣 <296550494@qq.com>
2020-11-16 19:33:00 +0800

    rn

---
+++ src/dateutils.js
@@ -0,0 +1,126 @@
+const XDate = require('xdate');
+
+function sameMonth(a, b) {
+  return a instanceof XDate && b instanceof XDate &&
+    a.getFullYear() === b.getFullYear() &&
+    a.getMonth() === b.getMonth();
+}
+
+function sameDate(a, b) {
+  return a instanceof XDate && b instanceof XDate &&
+    a.getFullYear() === b.getFullYear() &&
+    a.getMonth() === b.getMonth() &&
+    a.getDate() === b.getDate();
+}
+
+function isGTE(a, b) {
+  return b.diffDays(a) > -1;
+}
+
+function isLTE(a, b) {
+  return a.diffDays(b) > -1;
+}
+
+function fromTo(a, b) {
+  const days = [];
+  let from = +a, to = +b;
+  for (; from <= to; from = new XDate(from, true).addDays(1).getTime()) {
+    days.push(new XDate(from, true));
+  }
+  return days;
+}
+
+function month(xd) {
+  const year = xd.getFullYear(), month = xd.getMonth();
+  const days = new Date(year, month + 1, 0).getDate();
+
+  const firstDay = new XDate(year, month, 1, 0, 0, 0, true);
+  const lastDay = new XDate(year, month, days, 0, 0, 0, true);
+
+  return fromTo(firstDay, lastDay);
+}
+
+function weekDayNames(firstDayOfWeek = 0) {
+  let weekDaysNames = XDate.locales[XDate.defaultLocale].dayNamesShort;
+  const dayShift = firstDayOfWeek % 7;
+  if (dayShift) {
+    weekDaysNames = weekDaysNames.slice(dayShift).concat(weekDaysNames.slice(0, dayShift));
+  }
+  return weekDaysNames;
+}
+
+function formatToString(timestamp, formater) {
+  let date = new Date();
+  date.setTime(parseInt(timestamp));
+  formater = (formater != null) ? formater : 'yyyy-MM-dd hh:mm';
+  Date.prototype.Format = function (fmt) {
+    var o = {
+      "M+": this.getMonth() + 1, //月
+      "d+": this.getDate(), //日
+      "h+": this.getHours(), //小时
+      "m+": this.getMinutes(), //分
+      "s+": this.getSeconds(), //秒
+      "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+      "S": this.getMilliseconds() //毫秒
+    };
+
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o) {
+      if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ?
+        (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    }
+    return fmt;
+  };
+  return date.Format(formater);
+}
+
+function page(xd, firstDayOfWeek, showSixWeeks) {
+  const days = month(xd);
+  let before = [], after = [];
+
+  const fdow = ((7 + firstDayOfWeek) % 7) || 7;
+  const ldow = (fdow + 6) % 7;
+
+  firstDayOfWeek = firstDayOfWeek || 0;
+
+  const from = days[0].clone();
+  const daysBefore = from.getDay();
+
+  if (from.getDay() !== fdow) {
+    from.addDays(-(from.getDay() + 7 - fdow) % 7);
+  }
+
+  const to = days[days.length - 1].clone();
+  const day = to.getDay();
+  if (day !== ldow) {
+    to.addDays((ldow + 7 - day) % 7);
+  }
+
+  const daysForSixWeeks = (((daysBefore + days.length) / 6) >= 6);
+
+  if (showSixWeeks && !daysForSixWeeks) {
+    to.addDays(7);
+  }
+
+  if (isLTE(from, days[0])) {
+    before = fromTo(from, days[0]);
+  }
+
+  if (isGTE(to, days[days.length - 1])) {
+    after = fromTo(days[days.length - 1], to);
+  }
+
+  return before.concat(days.slice(1, days.length - 1), after);
+}
+
+module.exports = {
+  weekDayNames,
+  sameMonth,
+  sameDate,
+  month,
+  page,
+  fromTo,
+  isLTE,
+  isGTE,
+  formatToString
+};
